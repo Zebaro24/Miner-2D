c2c9174afcdb3a56f4766d29341769c6ad5ea8f1
WHITE <dima.itsko.ua@gmail.com>
2024-06-20 23:09:47 +0300

    розроблено і додано відображення текстур.

--- blocks/cobblestone.py
+++ blocks/cobblestone.py
@@ -1,35 +1,23 @@
-from config import COLOR
-
 import pygame
 from pygame import Surface
+from pygame.image import load as pygame_image_load  # импортируем метод загрузки изображения
 
-from block import Block
-
+from block import Block  # Предполагается, что у вас есть свой класс Block
+from config import COLOR
 
 class Cobblestone(Block):
     id = "cobblestone"
 
     @staticmethod
     def get_surface():
-        surface = Surface((80, 80))
+        # Загружаем изображение
+        image_path = 'images/cobblestone.png'
+        image = pygame_image_load(image_path).convert_alpha()
+
+
+        surface = Surface(image.get_size())
         surface.fill(COLOR.BACKGROUND)
 
-        dark_gray = (105, 105, 105)
-        light_gray = (192, 192, 192)
-        stones = [
-            (0, 0, 30, 30, dark_gray),
-            (20, 0, 30, 30, light_gray),
-            (60, 10, 25, 25, dark_gray),
-            (5, 30, 25, 25, light_gray),
-            (35, 35, 25, 25, dark_gray),
-            (65, 40, 30, 30, light_gray),
-            (10, 60, 20, 20, dark_gray),
-            (40, 55, 30, 30, light_gray),
-            (70, 65, 20, 20, dark_gray),
-        ]
-
-        for (x, y, w, h, color) in stones:
-            pygame.draw.rect(surface, color, (x, y, w, h), border_radius=10)
-        pygame.draw.rect(surface, COLOR.BLACK, (0, 0, 80, 80), 5)
+        surface.blit(image, (0, 0))
 
         return surface
--- blocks/player.py
+++ blocks/player.py
@@ -1,6 +1,15 @@
+import pygame
+
 from block import Block
 
 
 class Player(Block):
     id = "player"
+    image = pygame.image.load('cobblestone.png')
 
+    @staticmethod
+    def get_surface():
+        surface = Surface((80, 80))
+
+
+        return surface
