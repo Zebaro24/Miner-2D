a923e99cd3f3ffdf2e981b73dabe8ba2919f69af
Zebaro24 <zebaro24@stu.cn.ua>
2024-06-23 18:41:30 +0300

    Добавлена оптимизация

--- blocks/player_block.py
+++ blocks/player_block.py
@@ -8,20 +8,24 @@ class Player(Block):
 
     def set_down(self):
         self.image_path_player = "images/player/steve_face.png"
+        self.surface = None
 
     def set_up(self):
         self.image_path_player = "images/player/steve_back.png"
+        self.surface = None
 
     def set_left(self):
         self.image_path_player = "images/player/steve_left.png"
+        self.surface = None
 
     def set_right(self):
         self.image_path_player = "images/player/steve_right.png"
+        self.surface = None
 
     def get_surface(self):
-        self.surface = None
-        self.surface = super().get_surface()
-        image = pygame_image_load(self.image_path_player).convert_alpha()
-        self.surface.blit(image, (0, 0))
+        if self.surface is None:
+            self.surface = super().get_surface()
+            image = pygame_image_load(self.image_path_player).convert_alpha()
+            self.surface.blit(image, (0, 0))
 
         return self.surface
